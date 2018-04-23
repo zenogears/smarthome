3a02bd4fb0a37cbfaf8c0b9bcf0ca0756cb59749
zenogears <zenogears.aka@gmail.com>
2018-04-23 21:15:01 +0300

    12

--- app/getfunc.py
+++ app/getfunc.py
@@ -3,24 +3,12 @@ import sqlite3
 import sys
 import pytz, datetime
 
-<<<<<<< HEAD
 def timetoutc(mytime):
     UTC_OFFSET_TIMEDELTA = datetime.datetime.now() - datetime.datetime.utcnow()
     local_datetime = datetime.datetime.strptime(mytime, "%Y %m %d %H:%M:%S")
     result_utc_datetime = local_datetime + UTC_OFFSET_TIMEDELTA
-    
+
     return result_utc_datetime.strftime("%Y %m %d %H:%M:%S")
-=======
-
-
-def timetoutc(mytime):
-    local = pytz.timezone("Pacific/Truk")
-    naive = datetime.datetime.strptime(mytime, "%Y %m %d %H:%M:%S")
-    local_dt = local.localize(naive)
-    #utc_dt = local_dt.astimezone(pytz.utc)
-
-    return local_dt.strftime("%Y %m %d %H:%M:%S")
->>>>>>> 7aadf03475cd663874a2e34d6cc1607404838c24
 
 def getinfo():
   conn = sqlite3.connect('/home/pi/git/Web/mydatabase.db')
