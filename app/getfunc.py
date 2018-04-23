98b93966cf6ebefd59ecac60e26b281f8453d5a8
zenogears <zenogears.aka@gmail.com>
2018-04-23 20:57:24 +0300

    table

--- app/getfunc.py
+++ app/getfunc.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
 import sqlite3
+import sys
+import pytz, datetime
+
+local = pytz.timezone("Europe/Moscow")
+
+
+
+def timetoutc(mytime):
+
+    naive = datetime.datetime.strptime(mytime, "%Y %m %d %H:%M:%S")
+    local_dt = local.localize(naive)
+    utc_dt = local_dt.astimezone(pytz.utc)
+
+    return local_dt.strftime("%Y %m %d %H:%M:%S")
 
 def getinfo():
-  conn = sqlite3.connect("/home/pi/git/Web/mydatabase.db")
+  conn = sqlite3.connect('/home/zen/git/smarthome/mydatabase.db')
   cursor = conn.cursor()
 
   sql = "SELECT * FROM tempdata"
 
   cursor.execute(sql)
-  return(cursor.fetchall())
+  myfetch = cursor.fetchall()
+  returnfetch = []
+  for item in myfetch:
+    returnfetch.append((timetoutc(item[0]),item[1],item[2]))
+
+
+  return(returnfetch)
 
 if __name__ == '__main__':
   getinfo()
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app
+from flask import render_template
 from .getfunc import getinfo
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return str(getinfo())
+    return render_template("temp.html",
+        title = 'Home',
+        result = getinfo())
