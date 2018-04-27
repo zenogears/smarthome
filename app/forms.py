ef53f7cc3a64602dcf01bda9789226d2c2513923
zenogears <zenogears.aka@gmail.com>
2018-04-27 23:39:15 +0300

    updates

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,18 @@ class RegistrationForm(FlaskForm):
 class EditForm(FlaskForm):
     username = TextField('username', validators = [Required()])
     about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        FlaskForm.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not FlaskForm.validate(self):
+            return False
+        if self.username.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(username = self.username.data).first()
+        if user != None:
+            self.username.errors.append('This nickname is already in use. Please choose another one.')
+            return False
+        return True
